9c4f4d66670e288763f093229abbf1859d1c6268
josedeneto <josedenetojs@gmail.com>
2024-07-30 14:23:09 +0100

    Personalização do menu de opções e adição do pacote Iconsax

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:appchat_with_gemini/message_model.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -52,32 +53,35 @@ class _HomeViewState extends State<HomeView> {
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         child: Stack(
           children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 55),
-              child: ListView.builder(
-                reverse: true,
-                itemCount: messages.length,
-                itemBuilder: (context, m) {
-                  final messageTile = messages[m];
-                  return ListTile(
-                    title: Align(
-                      alignment: messageTile.isSentByme? Alignment.centerRight:Alignment.centerLeft,
-                      child: DecoratedBox(
-                        decoration: BoxDecoration(
-                          borderRadius: const BorderRadius.only(topLeft: Radius.circular(10),topRight: Radius.circular(10), bottomLeft:Radius.circular(10) ),
-                          color: messageTile.isSentByme?const Color(0XFF232323):Colors.transparent
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            messageTile.message,
-                            style: const TextStyle(color: Colors.white),
+            GestureDetector(
+              onTap: onItemTaped,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 55),
+                child: ListView.builder(
+                  reverse: true,
+                  itemCount: messages.length,
+                  itemBuilder: (context, m) {
+                    final messageTile = messages[m];
+                    return ListTile(
+                      title: Align(
+                        alignment: messageTile.isSentByme? Alignment.centerRight:Alignment.centerLeft,
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            borderRadius: const BorderRadius.only(topLeft: Radius.circular(10),topRight: Radius.circular(10), bottomLeft:Radius.circular(10) ),
+                            color: messageTile.isSentByme?const Color(0XFF232323):Colors.transparent
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              messageTile.message,
+                              style: const TextStyle(color: Colors.white),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               ),
             ),
            AnimatedPositioned(
@@ -92,12 +96,15 @@ class _HomeViewState extends State<HomeView> {
                 borderRadius: BorderRadius.circular(16)
                ),
                padding: const EdgeInsets.all(16),
-               child: Row(
+               child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                  children: [
-                   const Text(
-                     'Janela que aparece a partir da parte inferior',
-                     style: TextStyle(color: Colors.white),
+                   MenuItem(
+                    icon:Iconsax.image ,
+                    text: 'Galeria',
                    ),
+                   MenuItem(icon: Iconsax.document, text: 'Documento'),
+                   MenuItem(icon: Iconsax.audio_square, text: 'Áudio'),
                  ],
                ),
              ),
@@ -152,23 +159,33 @@ class _HomeViewState extends State<HomeView> {
                         fontWeight: FontWeight.normal,
                       ),
                       isDense: true,
-                      suffixIcon: GestureDetector(
-                        onTap: () {
-                          sendMessages();
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.all(6.0),
-                          child: CircleAvatar(
-                            maxRadius: 10,
-                            backgroundColor: isNoEmpty
-                                ? Colors.white
-                                : const Color.fromARGB(255, 37, 37, 37),
-                            child: Icon(
-                              Icons.arrow_upward_outlined,
-                              color: isNoEmpty ? Colors.black : Colors.grey,
+                      suffixIcon: Row(
+                        mainAxisSize: MainAxisSize.min,mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          if(inputController.text.isEmpty)
+                          GestureDetector(
+                            onTap: (){},
+                            child: const Icon(Icons.mic_none_rounded, color: Colors.white,size: 26,),),
+                            const SizedBox(width: 5,),
+                          GestureDetector(
+                            onTap: () {
+                              sendMessages();
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.all(6.0),
+                              child: CircleAvatar(
+                                maxRadius: 20,
+                                backgroundColor: isNoEmpty
+                                    ? Colors.white
+                                    : const Color.fromARGB(255, 37, 37, 37),
+                                child: Icon(
+                                  Icons.arrow_upward_outlined,
+                                  color: isNoEmpty ? Colors.black : Colors.grey,
+                                ),
+                              ),
                             ),
                           ),
-                        ),
+                        ],
                       ),
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(50),
@@ -183,6 +200,41 @@ class _HomeViewState extends State<HomeView> {
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class MenuItem extends StatelessWidget {
+ final IconData icon;
+ final String text;
+  const MenuItem({
+    super.key,
+    required this.icon,
+    required this.text
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+     onTap: (){},
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          DecoratedBox(
+           decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 26, 26, 26),
+             border: Border.all(color:const Color(0XFF343434) ),
+             borderRadius: BorderRadius.circular(10)
+           ),
+           child:  Padding(
+             padding: const EdgeInsets.all(16.0),
+             child:  Icon(icon, color: Colors.white,),
+           ),
+          ),
+          const SizedBox(height: 5,),
+          Text(text, style: const TextStyle(color: Colors.white, fontSize: 14),)
+        ],
       ),
     );
   }
