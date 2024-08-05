14c3b5a3d9a6d5be6fd61e5c7ec1c6bda5ece0c1
josedeneto <josedenetojs@gmail.com>
2024-08-05 18:08:47 +0100

    Fix: Renomeiei a classe UserService para UserServiceImpl

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'app/services/user_service_impl.dart';
 
 void main()async {
    WidgetsFlutterBinding.ensureInitialized();
-    final userService = UserService();
+    final userService = UserServiceImpl();
   await userService.initialize();
   runApp(const AppBoxAi());
 }
