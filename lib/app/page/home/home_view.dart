fc9a8f2dfb41d0f30a72cba1a3c3a39c6cdc1791
josedeneto <josedenetojs@gmail.com>
2024-08-05 12:05:18 +0100

    Personalizei as imagens selecionadas adicionando um botão para excluir dos selecionados

--- lib/app/page/home/home_view.dart
+++ lib/app/page/home/home_view.dart
@@ -29,7 +29,7 @@ class _HomeViewState extends State<HomeView> {
   bool showWindows = false;
   bool containsImage = false;
   bool showWelcomeMessage = true;
-  
+
   void onItemTaped() {
     setState(() {
       showWindows = !showWindows;
@@ -87,7 +87,6 @@ class _HomeViewState extends State<HomeView> {
   }
 
   void sendMessages() {
-    
     setState(() {
       messages.insert(
         0,
@@ -96,7 +95,7 @@ class _HomeViewState extends State<HomeView> {
             isSentByme: true,
             images: images?.map((image) => image.path).toList()),
       );
-     
+
       inputController.clear();
       images = [];
       showWelcomeMessage = false;
@@ -131,29 +130,29 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:AppColors.i.primary,
+      backgroundColor: AppColors.i.primary,
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
           child: Stack(
             children: [
-               if (showWelcomeMessage)
-               AiResponseMessageWidget(message: AppConstants.i.defaultMessageAI),
-                GestureDetector(
-                  onTap: onItemTaped,
-                  child: Padding(
-                    padding: const EdgeInsets.only(bottom: 55),
-                    child: ListView.builder(
-                      reverse: true,
-                      itemCount: messages.length,
-                      itemBuilder: (context, m) {
-                        final messageTile = messages[m];
-                        return MessageBoxTile(messageTile: messageTile);
-                      },
-                    ),
+              if (showWelcomeMessage)
+                AiResponseMessageWidget(
+                    message: AppConstants.i.defaultMessageAI),
+              GestureDetector(
+                onTap: onItemTaped,
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 55),
+                  child: ListView.builder(
+                    reverse: true,
+                    itemCount: messages.length,
+                    itemBuilder: (context, m) {
+                      final messageTile = messages[m];
+                      return MessageBoxTile(messageTile: messageTile);
+                    },
                   ),
                 ),
-              
+              ),
               AnimatedPositioned(
                 bottom: !showWindows && images != null ? 65 : -150,
                 duration: const Duration(milliseconds: 800),
@@ -171,11 +170,22 @@ class _HomeViewState extends State<HomeView> {
                                   const EdgeInsets.symmetric(horizontal: 2),
                               child: ClipRRect(
                                 borderRadius: BorderRadius.circular(8),
-                                child: Image.file(
-                                  File(i.path),
-                                  fit: BoxFit.cover,
-                                  width: 100,
-                                  height: 80,
+                                child: Stack(
+                                  children: [
+                                    Image.file(
+                                      File(i.path),
+                                      fit: BoxFit.cover,
+                                      width: 100,
+                                      height: 80,
+                                    ),
+                                    Positioned(
+                                    top: 2,
+                                    right: 2,
+                                    child: GestureDetector(
+                                    child: CircleAvatar(
+                                    radius: 8,
+                                    backgroundColor: AppColors.i.primary,child:  Icon(Icons.close, size: 12,color: AppColors.i.white,),),))
+                                  ],
                                 ),
                               ),
                             ),
@@ -185,73 +195,77 @@ class _HomeViewState extends State<HomeView> {
                   ),
                 ),
               ),
-               if (showWelcomeMessage)
-              Visibility(
-                visible: !showWindows,
-                child: Positioned(
-                  bottom:context.percentHeight(.1),
-                  child: Align(
-                    alignment: Alignment.center,
-                    child: SizedBox(
-                      width: MediaQuery.of(context).size.width,
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            width: 150,
-                            child: DecoratedBox(
-                              decoration: BoxDecoration(
-                                border: Border.all(color: AppColors.i.borderSide),
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                              child: Padding(
-                                padding: const EdgeInsets.all(6.0),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Icon(
-                                      Icons.light_mode_outlined,
-                                      color: AppColors.i.text,
-                                      size: 18,
-                                    ),
-                                    const SizedBox(
-                                      width: 3,
-                                    ),
-                                    Text(
-                                      'Exemplos',
-                                      style: AppTypography.i.light.copyWith(fontSize: 13),
-                                    )
-                                  ],
+              if (showWelcomeMessage)
+                Visibility(
+                  visible: !showWindows,
+                  child: Positioned(
+                    bottom: context.percentHeight(.1),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              width: 150,
+                              child: DecoratedBox(
+                                decoration: BoxDecoration(
+                                  border:
+                                      Border.all(color: AppColors.i.borderSide),
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(6.0),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Icon(
+                                        Icons.light_mode_outlined,
+                                        color: AppColors.i.text,
+                                        size: 18,
+                                      ),
+                                      const SizedBox(
+                                        width: 3,
+                                      ),
+                                      Text(
+                                        'Exemplos',
+                                        style: AppTypography.i.light
+                                            .copyWith(fontSize: 13),
+                                      )
+                                    ],
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          const SingleChildScrollView(
-                            scrollDirection: Axis.horizontal,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                SuggestedQuestion(
-                                  question: 'Roadmap 2024 para aprender Flutter',
-                                ),
-                                SuggestedQuestion(
-                                  question: 'Contar uma piada para se divertir',
-                                ),
-                                SuggestedQuestion(
-                                  question:
-                                      'Sugeria hábitos positivos para o bem-estar',
-                                )
-                              ],
+                            const SizedBox(
+                              height: 20,
                             ),
-                          ),
-                        ],
+                            const SingleChildScrollView(
+                              scrollDirection: Axis.horizontal,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  SuggestedQuestion(
+                                    question:
+                                        'Roadmap 2024 para aprender Flutter',
+                                  ),
+                                  SuggestedQuestion(
+                                    question:
+                                        'Contar uma piada para se divertir',
+                                  ),
+                                  SuggestedQuestion(
+                                    question:
+                                        'Sugeria hábitos positivos para o bem-estar',
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
               Menu(
                 showWindows: showWindows,
                 child: Row(
@@ -285,7 +299,7 @@ class _HomeViewState extends State<HomeView> {
                 child: SizedBox(
                   width: MediaQuery.sizeOf(context).width,
                   child: ColoredBox(
-                    color:AppColors.i.primary,
+                    color: AppColors.i.primary,
                     child: TextFormField(
                       controller: inputController,
                       style: const TextStyle(color: Colors.white),
@@ -294,13 +308,12 @@ class _HomeViewState extends State<HomeView> {
                           inputController.text = value;
                         });
                       },
-                     
                       decoration: InputDecoration(
                         prefixIcon: GestureDetector(
                           onTap: () {
                             onItemTaped();
                           },
-                          child:  Icon(
+                          child: Icon(
                             Iconsax.attach_circle,
                             color: AppColors.i.text,
                             size: 23,
@@ -314,7 +327,7 @@ class _HomeViewState extends State<HomeView> {
                             if (inputController.text.isEmpty)
                               GestureDetector(
                                 onTap: () {},
-                                child:  Icon(
+                                child: Icon(
                                   Icons.mic_none_rounded,
                                   color: AppColors.i.text,
                                   size: 26,
@@ -334,13 +347,13 @@ class _HomeViewState extends State<HomeView> {
                                 padding: const EdgeInsets.all(6.0),
                                 child: CircleAvatar(
                                   radius: 20,
-                                  backgroundColor: inputController
-                                              .text.isNotEmpty ||
-                                          images!.isNotEmpty
-                                      ? AppColors.i.text
-                                      : AppColors.i.disableButton,
-                                  child: Icon(Iconsax.arrow_up_3,
-                                   // Icons.arrow_upward_outlined,
+                                  backgroundColor:
+                                      inputController.text.isNotEmpty ||
+                                              images!.isNotEmpty
+                                          ? AppColors.i.text
+                                          : AppColors.i.disableButton,
+                                  child: Icon(
+                                    Iconsax.arrow_up_3,
                                     color: inputController.text.isNotEmpty ||
                                             images!.isNotEmpty
                                         ? AppColors.i.primary
