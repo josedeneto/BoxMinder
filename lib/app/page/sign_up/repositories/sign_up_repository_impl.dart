19b1b55c94c353bfb8607a881cb981f3cd351065
josedeneto <josedenetojs@gmail.com>
2024-08-08 12:45:55 +0100

    Injeção de dependência na class repository

--- lib/app/page/sign_up/repositories/sign_up_repository_impl.dart
+++ lib/app/page/sign_up/repositories/sign_up_repository_impl.dart
@@ -1,13 +1,17 @@
-
 import 'package:appchat_with_gemini/app/model/user_model.dart';
 import 'package:appchat_with_gemini/app/page/sign_up/repositories/sign_up_repository_i.dart';
+import 'package:appchat_with_gemini/app/services/user_service_i.dart';
 
 class SignUpRepositoryImpl implements SignUpRepositoryI {
-
+  final UserServiceI userService;
+  SignUpRepositoryImpl({
+    required this.userService,
+  });
   
   @override
-  Future<void> signUp(UserModel userModel) {
-    throw UnimplementedError();
+  Future<void> signUp(UserModel userModel) async {
+    
+  await  userService.setUserLoggedIn();
   }
   
 }
